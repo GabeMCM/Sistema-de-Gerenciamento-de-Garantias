bf75ca94782b82d8ac9687983c677f462b121c12
GabeMCM <gabrielmateuscm@gmail.com>
2023-05-29 10:44:08 -0300

    style/organized/show data in screen

--- public/script.js
+++ public/script.js
@@ -1,4 +1,10 @@
 const openForm = () => {
-    const form = document.querySelector(".form-box");
-    form.classList.toggle('toggleForm');
-}
+	const form = document.querySelector(".form-box");
+	const button = document.querySelector(".open-form-button");
+	const header = document.querySelector("header");
+	setTimeout(() => {
+		button.classList.toggle("rotate");
+	}, 200);
+	form.classList.toggle("openForm");
+	header.classList.toggle("header-with-form-open");
+};
