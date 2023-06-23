83001cd813750f2d4b2d3b73cbba03a686dc2987
GabeMCM <gabrielmateuscm@gmail.com>
2023-06-23 12:08:20 -0300

    show information ok - css changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,17 @@
+const buttonShowData = document.querySelectorAll(".buttonShowData");
+
+buttonShowData.forEach((button) => {
+	console.log(button);
+	button.addEventListener("click", () => {
+		const divPai = button.parentElement;
+		const li = divPai.parentElement;
+		const info = li.querySelector("#hiddenInformation");
+		li.classList.toggle("openLi");
+		info.classList.toggle("displayInformation");
+		divPai.classList.toggle("resizeButton")
+	});
+});
+
 const openForm = () => {
 	const form = document.querySelector(".form-box");
 	const button = document.querySelector(".open-form-button");
@@ -9,4 +23,3 @@ const openForm = () => {
 	form.classList.toggle("openForm");
 	header.classList.toggle("header-with-form-open");
 };
-
